d3f825bcc0f8402b60247c22822e2075944a7c5f
IvanovAA <ivanov_alexandr_05@mail.ru>
2025-03-30 23:53:27 +0300

    for site

--- main.js
+++ main.js
@@ -99,71 +99,71 @@ function get_input ()
 
 function run(input) 
 {
-    var sm = 0, i = 1, j, n = 1000;
-    var start = performance.now();
-    for (var i = 1; i <= n; i += 1)
-    {
-        for (var j = 1; j <= n; j += 1)
-        {
-            sm += 1 / (i * j);
-        }
-    }
-    console.log(sm);
-    console.log(performance.now() - start);
+    // var sm = 0, i = 1, j, n = 1000;
+    // var start = performance.now();
+    // for (var i = 1; i <= n; i += 1)
+    // {
+    //     for (var j = 1; j <= n; j += 1)
+    //     {
+    //         sm += 1 / (i * j);
+    //     }
+    // }
+    // console.log(sm);
+    // console.log(performance.now() - start);
 
     console.log("Running:\n" + input);
     clearOutput();
 
-    lexer = new Lexer(input);
-    tokens = lexer.get_tokens();
+    // lexer = new Lexer(input);
+    // tokens = lexer.get_tokens();
 
-    console.log(tokens);
+    // console.log(tokens);
 
-    parser = new Parser(tokens);
-    parse_tree = parser.parse();
+    // parser = new Parser(tokens);
+    // parse_tree = parser.parse();
 
-    console.log(parse_tree);
+    // console.log(parse_tree);
 
-    program = new Program(parse_tree);
-    program.run();
+    // program = new Program(parse_tree);
+    // program.run();
+    
+    try
+    {
+        lexer = new Lexer(input);
+        tokens = lexer.get_tokens();
+    }
+    catch (error)
+    {
+        addOutput("Lexical error: " + error.message, true);
+        return;
+    }
+    
+    try
+    {
+        console.log(tokens);
+        
+        parser = new Parser(tokens);
+        parse_tree = parser.parse();
+        
+        console.log(parse_tree);
+        
+        program = new Program(parse_tree);
+    }
+    catch (error)
+    {
+        addOutput("Semantic error: " + error.message, true);
+        return;
+    }
+    
+    try
+    {
+        program.run();
+    }
+    catch (error)
+    {
+        addOutput("Runtime error: " + error.message, true);
+        return;
+    }
 
     console.log(`time: ${Math.round(program.overall_time * 1000) / 1000}ms`);
-
-    // try
-    // {
-    //     lexer = new Lexer(input);
-    //     tokens = lexer.get_tokens();
-    // }
-    // catch (error)
-    // {
-    //     addOutput("Lexical error: " + error.message, true);
-    //     return;
-    // }
-
-    // try
-    // {
-    //     console.log(tokens);
-
-    //     parser = new Parser(tokens);
-    //     parse_tree = parser.parse();
-
-    //     console.log(parse_tree);
-
-    //     program = new Program(parse_tree);
-    // }
-    // catch (error)
-    // {
-    //     addOutput("Semantic error: " + error.message, true);
-    //     return;
-    // }
-
-    // try
-    // {
-    //     program.run();
-    // }
-    // catch (error)
-    // {
-    //     addOutput("Runtime error: " + error.message, true);
-    //     return;
-    // }
 }
